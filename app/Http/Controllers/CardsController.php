3266c812ed8aef87f50e02357fd54a091a9e7495
Alberto Fraile <albertofraile9@gmail.com>
2022-02-17 11:59:59 +0100

    NuevosArreglos

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Card;
+use App\Models\Collection;
+use App\Models\CardSold;
+use App\Models\User;
 use Illuminate\Support\MessageBag;
 use Illuminate\Support\Facades\Log;
 
@@ -49,6 +52,103 @@ class CardsController extends Controller
         return response()->json($response);
     }
 
+    public function createCollection(Request $req){
+
+        $response = ["status" => 1, "msg" => ""];
+
+        $validator = validator::make(json_decode($req->getContent(),true
+        ), 
+            ['name' => 'required|max:30',
+             'symbol' => 'required|max:30',
+             'date' => 'required|max:30'
+            ]
+        );
+
+        if ($validator->fails()){
+            $response['status'] = 0;
+            $response['msg'] = $validator->errors();
+
+        }else {
+            $datos = $req->getContent();
+            $datos = json_decode($datos);
+
+            $collection = new Collection();
+
+            $collection->name = $datos->name;
+            $collection->symbol = $datos->symbol;
+            $collection->date = $datos->date;
+
+            try{
+                $collection->save();
+                $response['msg'] = "Collection save with id ".$collection->id;
+            }catch(\Exception $e){
+                $response['status'] = 0;
+                $response['msg'] = "An error has occurred: ".$e->getMessage();
+            }
+        }
+        return response()->json($response);
+    }
+
+    public function createCardsSolds(Request $req){
+
+        $response = ["status" => 1, "msg" => ""];
+
+        $validator = validator::make(json_decode($req->getContent(),true
+        ), 
+            ['name' => 'required|max:30',
+             'amount' => 'required|max:30',
+             'price' => 'required|max:30'
+            ]
+        );
+
+        if ($validator->fails()){
+            $response['status'] = 0;
+            $response['msg'] = $validator->errors();
+
+        }else {
+            $datos = $req->getContent();
+            $datos = json_decode($datos);
+
+            $card = new CardSold();
+
+            $card->name = $datos->name;
+            $card->amount = $datos->amount;
+            $card->price = $datos->price;
+
+            try{
+                $card->save();
+                $response['msg'] = "Card save with id ".$card->id;
+            }catch(\Exception $e){
+                $response['status'] = 0;
+                $response['msg'] = "An error has occurred: ".$e->getMessage();
+            }
+        }
+        return response()->json($response);
+    }
+
+    // if(isset($datos->user_asociate) && !empty($datos->user_asociate) && isset($datos->card_asociate) && !empty($datos->card_asociate) ){
+    //     $user = User::find($datos->user_asociate);
+    //     $card = Carta::find($datos->card_asociate);
+    //     if($user && $card){
+    //         $card -> user_asociate = $datos->user_asociate;
+    //         $card -> card_asociate = $datos->card_asociate;
+    //         try {
+    //             $card->save();
+    //             $respuesta["msg"] = "Venta subida correctamente";
+    //         }catch (\Exception $e) {
+    //             $respuesta["status"] = 0;
+    //             $respuesta["msg"] = "Se ha producido un error".$e->getMessage();  
+    //         }  
+    //     } else {
+    //         $respuesta["status"] = 0;
+    //         $respuesta["msg"] = "Usuario o carta no encontrada";
+    //     }
+    // }
+    // else {
+    //     $respuesta["status"] = 0;
+    //     $respuesta["msg"] = "No se ha asociado ningun usuario o carta, vuelve a intentarlo";
+    // }
+
     public function searchCard(Request $req){
         $response = ["status" => 1, "msg" => ""];
         $datos = $req->getContent();
@@ -77,5 +177,28 @@ class CardsController extends Controller
            Log::error('An error has occurred: ');          
         }
         return response()->json($response);
+    }    
+
+    public function searchBuyCard(Request $req){
+        $response = ["status" => 1, "msg" => ""];
+        $datos = $req->getContent();
+        
+        $datos = json_decode($datos);
+
+        try{
+            //$card = $card->makeHidden(['id','created_at','updated_at']);
+            $card = DB::Table('card_solds')
+            ->where('name', 'like', '%' .$req->input('name'). '%')
+            ->select('card_solds.*', 'users.name')
+            ->orderBy('card_solds.price', 'asc')
+            ->get();
+            $response['datos'] = $card; 
+
+        }catch(\Exception $e){
+           $response['status'] = 0;
+           $response['msg'] = "An error has occurred: ".$e->getMessage();           
+        }
+        return response()->json($response);
     }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -26,17 +26,17 @@ Route::prefix('user')->group(function(){
 	Route::post('/login',[UsersController::class,'login']);
 	Route::post('/recoveredPassword',[UsersController::class,'recoveredPassword']);
 	Route::put('/register',[UsersController::class,'register']);
-	Route::get('/searchBuyCard',[CardsSoldsController::class,'searchBuyCard']);
+	Route::get('/searchBuyCard',[CardsController::class,'searchBuyCard']);
 	
 	Route::middleware('api_token','validation_admin')->group(function(){
 		Route::put('/createCard',[CardsController::class,'createCard']);
-		Route::put('/createCollection',[CollectionController::class,'createCollection']);
+		Route::put('/createCollection',[CardsController::class,'createCollection']);
 		Route::put('/asociate_cards/{cards_id}/{collections_id}',[UsersController::class,'asociate_cards']);
 	});
 
 	Route::middleware('api_token', 'validation')->group(function(){
 		Route::get('/searchCard',[CardsController::class,'searchCard']);
-		Route::put('/createCardsSolds',[CardsSoldsController::class,'createCardsSolds']);
+		Route::put('/createCardsSolds',[CardsController::class,'createCardsSolds']);
 	});
 });
 	
